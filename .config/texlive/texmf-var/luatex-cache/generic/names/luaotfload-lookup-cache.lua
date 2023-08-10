e76dd76a27785230ddd248854d01af39eccdd524
markus-langgeng <markus.langgeng4@gmail.com>
2023-08-11 00:47:43 +0700

    add texlive and zathura conf directory

---
+++ .config/texlive/texmf-var/luatex-cache/generic/names/luaotfload-lookup-cache.lua
@@ -0,0 +1,14 @@
+return {
+ ["ARPLUKaiCN/B:mode=node;script=latn;language=dflt;#b#690709"]={ "/usr/share/fonts/TTF/ukai.ttc", 1 },
+ ["ARPLUKaiCN/BI:mode=node;script=latn;language=dflt;#bi#690709"]={ "/usr/share/fonts/TTF/ukai.ttc", 1 },
+ ["ARPLUKaiCN/I:mode=node;script=latn;language=dflt;#i#690709"]={ "/usr/share/fonts/TTF/ukai.ttc", 1 },
+ ["ARPLUKaiCN:mode=node;#690706"]={ "/usr/share/fonts/TTF/ukai.ttc", 1 },
+ ["ARPLUKaiCN:mode=node;#690710"]={ "/usr/share/fonts/TTF/ukai.ttc", 1 },
+ ["ARPLUKaiCN:mode=node;script=latn;language=dflt;#690709"]={ "/usr/share/fonts/TTF/ukai.ttc", 1 },
+ ["ARPLUKaiCN:mode=node;script=latn;language=dflt;#690710"]={ "/usr/share/fonts/TTF/ukai.ttc", 1 },
+ ["ARPLUKaiCN:mode=node;script=latn;language=dflt;jfm=zh_CN/{quanjiao}#1184071"]={ "/usr/share/fonts/TTF/ukai.ttc", 1 },
+ ["ARPLUKaiCN:mode=node;script=latn;language=dflt;jfm=zh_CN/{quanjiao}#393216"]={ "/usr/share/fonts/TTF/ukai.ttc", 1 },
+ ["ARPLUKaiCN:mode=node;script=latn;language=dflt;jfm=zh_CN/{quanjiao}#524288"]={ "/usr/share/fonts/TTF/ukai.ttc", 1 },
+ ["ARPLUKaiCN:mode=node;script=latn;language=dflt;jfm=zh_CN/{quanjiao}#690708"]={ "/usr/share/fonts/TTF/ukai.ttc", 1 },
+ ["ARPLUKaiCN:mode=node;script=latn;language=dflt;jfm=zh_CN/{quanjiao}#789381"]={ "/usr/share/fonts/TTF/ukai.ttc", 1 },
+}
